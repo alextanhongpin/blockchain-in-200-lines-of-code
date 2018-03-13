95845c847d3aa88a6248a383437de41ac3ce866c
alextanhongpin <alextan@seekasia.com>
2018-03-13 21:04:52 +0800

    refactor: change to class

--- index.js
+++ index.js
@@ -1,41 +1,110 @@
 
-// https://cdnjs.cloudflare.com/ajax/libs/crypto-js/3.1.9-1/crypto-js.min.js
+const CryptoJS = require('crypto-js')
+
 class Block {
-	constructor(index, previousHash, timestamp, data, hash) {
-		this.index = index
-		this.previousHash = previousHash
-		this.timestamp = timestamp
-		this.data = data
-		this.hash = hash.toString()
-	}
+  constructor (index, previousHash, timestamp, data, hash) {
+    this.index = index
+    this.previousHash = previousHash
+    this.timestamp = timestamp
+    this.data = data
+    this.hash = hash.toString()
+  }
+
+  static calculateHashForBlock ({index, previousHash, timestamp, data}) {
+    return CryptoJS.SHA256(index + previousHash + timestamp + data).toString()
+  }
 }
 
-function calculateHash (index, previousHash, timestamp, data) {
-	return CryptoJS.SHA256(index + previousHash + timestamp + data).toString()
+class Blockchain {
+  constructor () {
+    this.blockchain = [this._getGenesisBlock()]
+  }
+
+  _getGenesisBlock () {
+    return new Block(0, '0', 1465154705, 'my block', '816534932c2b7154836da6afc367695e6337db8a921823784c14378abed4f7d7')
+  }
+
+  static isValidNewBlock (newBlock, previousBlock) {
+    if (previousBlock.index + 1 !== newBlock.index) {
+      console.log('invalid index')
+      return false
+    } else if (previousBlock.hash !== newBlock.previousHash) {
+      console.log('invalid previoushash')
+      return false
+    } else if (Block.calculateHashForBlock(newBlock) !== newBlock.hash) {
+      console.log(`invalid hash: ${Block.calculateHashForBlock()}`)
+      return false
+    }
+    return true
+  }
+
+  addBlock (newBlock) {
+    if (Blockchain.isValidNewBlock(newBlock, this.getLatestBlock())) {
+      this.blockchain.push(newBlock)
+    }
+  }
+
+  generateNextBlock (blockData) {
+    const previousBlock = this.getLatestBlock()
+    const nextIndex = previousBlock.index + 1
+    const nextTimestamp = new Date().getTime() / 10000
+    const nextHash = Block.calculateHashForBlock({
+      index: nextIndex,
+      previousHash: previousBlock.hash,
+      timestamp: nextTimestamp,
+      data: blockData
+    })
+    return new Block(nextIndex, previousBlock.hash, nextTimestamp, blockData, nextHash)
+  }
+
+  getLatestBlock () {
+    return this.blockchain[this.blockchain.length - 1]
+  }
+
+  replaceChain (newBlocks) {
+    if (Blockchain.isValidChain(newBlocks) && newBlocks.length > this.blockchain.length) {
+      console.log('Received blockchain is valid. Replacing current blockchain with received blockchain.')
+      this.blockchain = newBlocks
+      console.log(this.responseLatestMsg())
+    } else {
+      console.log('Received blockchain invalid')
+    }
+  }
+  static isValidChain (blockchainToValidate) {
+    if (JSON.stringify(blockchainToValidate[0]) !== JSON.stringify(getGenesisBlock())) {
+      return false
+    }
+
+    const tempBlocks = [blockchainToValidate[0]]
+    for (let i = 0; i < blockchainToValidate.length; i += 1) {
+      if (Blockchain.isValidNewBlock(blockchainToValidate[i], tempBlocks[i - 1])) {
+        tempBlocks.push(blockchainToValidate[i])
+      } else {
+        return false
+      }
+    }
+    return true
+  }
+  responseLatestMsg () {
+    return {
+      type: 2,
+      data: JSON.stringify([this.getLatestBlock()])
+    }
+  }
 }
 
-function generateNextBlock(blockData) {
-	const previousBlock = getLatestBlock()
-	const nextIndex = previousBlock.index + 1
-	const nextTimestamp = new Date().getTime() / 10000
-	const nextHash = calculateHash(nextIndex, previousBlock.hash, nextTimestamp, blockData)
-	return new Block(nextIndex, previousBlock.hash, nextTimestamp, blockData, nextHash)
+function main () {
+  const blockchain = new Blockchain()
+
+  const newBlock = blockchain.generateNextBlock('hello world')
+  blockchain.addBlock(newBlock)
+  console.log(blockchain.responseLatestMsg())
+  console.log(JSON.stringify(newBlock))
+
+  const newBlock2 = blockchain.generateNextBlock('hi')
+  blockchain.addBlock(newBlock2)
+  console.log(blockchain.responseLatestMsg())
+  console.log(JSON.stringify(newBlock2))
 }
 
-function getGenesisBlock() {
-	return new Block(0, "0", 1465154705, "my block", "816534932c2b7154836da6afc367695e6337db8a921823784c14378abed4f7d7")
-}
-
-const blockchain = [getGenesisBlock()]
-
-function isValidNewBlock(newBlock, previousBlock) {
-	if (previousBlock.index + 1 !== newBlock.index) {
-		console.log('invalid index')
-		return false
-	} else if (previousBlock.hash !== newBlock.previousHash){
-		console.log('invalid previoushash')
-		return false
-	} else if (calculateHashForBlock(newBlock) !== newBlock.hash)  {
-		console.log(`invalid hash: ${calculateHashForBlock()}`)
-	}
-}
+main()
